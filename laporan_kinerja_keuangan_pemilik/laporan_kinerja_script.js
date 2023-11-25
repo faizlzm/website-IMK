e1cf87be085f3a7183d9740596c7bd9b1bd46976
faizlzm <faiz.ali.chanel@gmail.com>
2023-11-25 17:30:40 +0700

    a

--- laporan_kinerja_keuangan_pemilik/laporan_kinerja_script.js
+++ laporan_kinerja_keuangan_pemilik/laporan_kinerja_script.js
@@ -26,28 +26,143 @@ allLinks.forEach((elem) => {
 });
 
 // Halaman yang ditampilkan saat salah satu dropbox ditekan
+document.getElementById('jenis_laporan').addEventListener('change', function () {
+  var selectedValue = this.value;
 
-function showDiv() {
-  var dropdown = document.getElementById("jenis_laporan");
-  var selectedValue = dropdown.options[dropdown.selectedIndex].value;
-  var divToShow = document.getElementById("laporan-pilihan");
+  // Menyembunyikan semua div di dalam .laporan-pilihan
+  var laporanPilihanDivs = document.querySelectorAll('.laporan-pilihan div');
+  laporanPilihanDivs.forEach(function (div) {
+      div.classList.remove('active');
+  });
 
-  // Cek nilai yang dipilih
-  if (selectedValue === "Laporan_1") {
-    // Tampilkan div dan sesuaikan kontennya
-    divToShow.style.display = "block";
-    divToShow.innerHTML = '<div class="chart-container">' +
-    '<div class="bar" style="height: 80px;">80</div>' +
-    '<div class="bar" style="height: 120px;">120</div>' +
-    '<div class="bar" style="height: 50px;">50</div>' +
-    '<div class="bar" style="height: 90px;">90</div>' +
-    '</div>';
-  } else if (selectedValue === "Laporan_2") {
-    // Tampilkan div dan sesuaikan kontennya
-    divToShow.style.display = "block";
-    divToShow.innerHTML = "Konten untuk Pesanan Laundry";
-  } else {
-    // Sembunyikan div jika tidak ada yang dipilih
-    divToShow.style.display = "none";
+  // Menampilkan div yang sesuai dengan opsi yang dipilih
+  var selectedDiv = document.getElementById(selectedValue);
+  if (selectedDiv) {
+      selectedDiv.classList.add('active');
   }
-}
+});
+
+// grafik
+/* Get CSS Variables */
+const colorPrimary = getComputedStyle(document.documentElement)
+  .getPropertyValue("--color-primary")
+  .trim();
+
+const colorDefault = getComputedStyle(document.documentElement)
+  .getPropertyValue("--color-default")
+  .trim();
+
+const colorLabel = getComputedStyle(document.documentElement)
+  .getPropertyValue("--color-label")
+  .trim();
+
+const fontFamily = getComputedStyle(document.documentElement)
+  .getPropertyValue("--font-family")
+  .trim();
+
+/* Declare Default Chart Options */
+
+const defaultOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    selection: {
+      enabled: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    width: "100%",
+    height: 150,
+    offsetY: 8,
+  },
+  stroke: {
+    lineCap: "round",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legend: {
+    show: false,
+  },
+  states: {
+    hover: {
+      filter: {
+        type: "none",
+      },
+    },
+  },
+};
+
+// Bar Chart
+
+var barOptions = {
+  ...defaultOptions,
+  chart: {
+    ...defaultOptions.chart,
+    type: "bar",
+  },
+  tooltip: {
+    enabled: true,
+    fillSeriesColor: false,
+    style: {
+      fontFamily: fontFamily,
+    },
+    y: {
+      formatter: (value) => {
+        return `${value}K`;
+      },
+    },
+  },
+  series: [
+    {
+      name: "This Year",
+      data: [23, 55, 41, 37, 22, 43],
+    },
+    {
+      name: "Last Year",
+      data: [34, 32, 33, 52, 13, 54],
+    },
+  ],
+  colors: [colorPrimary, colorDefault],
+  stroke: { show: false },
+  grid: {
+    borderColor: "rgba(0, 0, 0, 0)",
+    padding: { left: 0, right: 0, top: -16, bottom: -8 },
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "50%",
+      borderRadius: 6,
+    },
+  },
+  yaxis: {
+    show: false,
+  },
+  xaxis: {
+    labels: {
+      floating: true,
+      show: true,
+      style: {
+        fontFamily: fontFamily,
+        colors: colorLabel,
+      },
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+    crosshairs: {
+      show: false,
+    },
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  },
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barOptions);
+
+barChart.render();
